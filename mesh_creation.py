6a54e78ad3b65321d63414dfd66d0ec81c85b0f1
Mathis Grillot <mathis.grillot@etu.minesparis.psl.eu>
2025-04-02 15:45:10 +0200

    eej

---
+++ mesh_creation.py
@@ -0,0 +1,24 @@
+import open3d as o3d
+import numpy as np
+import time
+
+
+# Charger le nuage de points
+pcd = o3d.io.read_point_cloud("nuage_oriente.ply")
+k = 150  # Nombre de voisins les plus proches
+
+pcd.orient_normals_consistent_tangent_plane(k)
+
+# Créer le maillage 3D à partir du nuage de points
+mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=12)
+
+# Supprimer les sommets dont la densité est inférieure au seuil
+densities = np.asarray(densities)
+seuil = np.percentile(densities, 2)
+mesh.remove_vertices_by_mask(densities < seuil)
+
+mesh.orient_triangles()
+mesh.compute_vertex_normals()
+
+# Visualiser le maillage avec les normales
+o3d.visualization.draw_geometries([mesh], point_show_normal=True)
