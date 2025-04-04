619f67938e83fb0b858b659d8f2ffb367e9d5836
Mathis Grillot <mathis.grillot@etu.minesparis.psl.eu>
2025-04-04 16:25:01 +0200

    oihoihyi

--- fichiers_ply/mesh_creation.py
+++ fichiers_ply/mesh_creation.py
@@ -4,17 +4,17 @@ import time
 
 
 # Charger le nuage de points
-pcd = o3d.io.read_point_cloud("nuage_oriente.ply")
+pcd = o3d.io.read_point_cloud("fichiers_ply/nuage_oriente.ply")
 k = 150  # Nombre de voisins les plus proches
 
 pcd.orient_normals_consistent_tangent_plane(k)
 
 # Créer le maillage 3D à partir du nuage de points
-mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=12)
+mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=8)
 
 # Supprimer les sommets dont la densité est inférieure au seuil
 densities = np.asarray(densities)
-seuil = np.percentile(densities, 2)
+seuil = np.percentile(densities, 44)
 mesh.remove_vertices_by_mask(densities < seuil)
 
 mesh.orient_triangles()
@@ -22,4 +22,4 @@ mesh.compute_vertex_normals()
 
 # Visualiser le maillage avec les normales
 o3d.visualization.draw_geometries([mesh], point_show_normal=True)
-o3d.io.write_triangle_mesh("mesh_cailloux.ply", mesh)
+o3d.io.write_triangle_mesh("fichiers_ply/mesh_cailloux_min.ply", mesh)
