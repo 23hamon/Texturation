b791e2b1ad2dd5c9225a9ede4db499987da600de
Mathis Grillot <mathis.grillot@etu.minesparis.psl.eu>
2025-04-14 14:25:36 +0200

    la fonction de cout calculée mamène

---
+++ cost_wij.py
@@ -0,0 +1,75 @@
+import json
+import numpy as np
+import cv2
+from tqdm import tqdm
+import trimesh
+from distance_obj_camera import dist_obj_cam
+from data import param_calib
+from utils import get_image_data
+
+with open("data/absolute_transforms_full.json", "r") as f:
+    camera_data = json.load(f)["0"]
+
+mesh = trimesh.load_mesh('fichiers_ply/mesh_cailloux_low.ply')
+Mij = np.load("fichiers_intermediaires/Mij.npy")
+R_cam_l_r = param_calib.R_DG
+t_cam_l_r = param_calib.t_DG
+n_l = param_calib.n_l
+
+#initialisation
+start, stop = 5001, 5002
+mesh.visual.vertex_colors = [200, 200, 200, 255]
+mesh.visual.face_colors[start:stop] = [255, 0, 0, 255]
+
+transforms = []
+for key in camera_data:
+    r, t = get_image_data(key)
+    R, _ = cv2.Rodrigues(r)
+    transforms.append((R, t))
+
+# function 
+def cost_function(normal_face, normal_view, X, R_cam, t_cam, R_cam_l_r, t_cam_l_r):
+    d = dist_obj_cam(X, R_cam, t_cam, R_cam_l_r, t_cam_l_r)
+    return np.dot(normal_face, normal_view) * d[1]
+
+#calcul de la matrice de cout
+cost_matrix = np.full((len(mesh.faces), len(camera_data)), np.inf)
+
+for i in tqdm(range(len(mesh.faces))):
+    X = mesh.triangles_center[i]
+    normal_face = mesh.face_normals[i]
+    for j, key in enumerate(camera_data):
+        if Mij[i, int(key) - 1] == 1: #si face visible
+            R, T = transforms[j]
+            normal_view = - R.T @ n_l
+            cost_matrix[i, int(key) - 1] = cost_function(normal_face, normal_view, X, R, T, R_cam_l_r, t_cam_l_r)
+
+#visualisation
+scene = mesh.scene()
+scene.add_geometry(mesh)
+lines = trimesh.load_path(mesh.vertices[mesh.edges_unique])
+lines.colors = [[0, 0, 0, 255]] * len(lines.entities)
+scene.add_geometry(lines)
+
+i = 500 #par exemple
+center = mesh.triangles_center[i]
+normal = mesh.face_normals[i]
+line = trimesh.load_path([center, center + 50 * normal])
+line.colors = [[255, 0, 0, 255]]
+scene.add_geometry(line)
+
+for j, view in enumerate(camera_data):
+    if Mij[i, int(view) - 1] == 1:
+        r, t = get_image_data(view)
+        R, _ = cv2.Rodrigues(r)
+        normal_view = - R.T @ n_l
+        line = trimesh.load_path([center, center + 50 * normal_view])
+        line.colors = [[0, 255, 0, 255]]
+        scene.add_geometry(line)
+
+scene.show()
+
+print(Mij[i, :])
+print(cost_matrix[i, :])
+
+Wij = np.save("fichiers_intermediaires/Wij.npy", cost_matrix)
