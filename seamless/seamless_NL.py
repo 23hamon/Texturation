c26d85615bb60b5cc6abd15fecacaf164fce0a24
Mathis Grillot <mathis.grillot@etu.minesparis.psl.eu>
2025-04-28 17:02:10 +0200

    jpp

--- seamless/seamless_NL.py
+++ seamless/seamless_NL.py
@@ -23,6 +23,7 @@ views = {0: Vue1, 1: Vue2, 2: Vue3}
 n_views = len(views)
 n_faces = len(mesh.faces)
 
+
 # Best views ici déterminé
 best_views = [0, 2]
 
@@ -60,36 +61,32 @@ def intensity(view_id, color_channel):
         uvs_coords = uvs[face]
         c = (uvs_coords[:, 0] * w).astype(int)
         r = (uvs_coords[:, 1] * h).astype(int)
+        c = np.clip(c, 0, w - 1)
+        r = np.clip(r, 0, h - 1)
         rr, cc = polygon(r, c)
         for y, x in zip(rr, cc):
-            if 0 <= x < w and 0 <= y < h:
-                f[y, x] = image[int(y), int(x), color_channel]
+            f[y, x] = image[int(y), int(x), color_channel]
     return f
 
 # plt.figure(figsize=(6,6))
-# im = plt.imshow(intensity(0, 1), origin='lower')
+# im = plt.imshow(intensity(2, 0), origin='lower')
 # plt.axis('off')
-# plt.title('Intensité verte Vue 0')
+# plt.title('Intensité VERTE Vue 2')
 # plt.colorbar(im, label="Intensité")
 # plt.show()
+print(intensity(0, 0)[0,0])
 
+def get_intensity_vertice(view_id, vertex_id, color_channel):
+    image_size = views[view_id].shape[:2]
+    uvs = uvs[mesh.faces[vertex_id]]
+    uvs = uvs * ima
+    f = intensity(view_id, color_channel)
+    print(x, y)
+    return f[y, x]
+    
 
-def get_intensity_vertice(f, view_id, vertex_id):
-    h, w = f.shape
-    for face_id, face in enumerate(mesh.faces):
-        if best_views[face_id] != view_id:
-            continue
-        for vertex_id in face:
-            local_index = np.where(face == vertex_id)[0][0]
-            uv = uvs[face[local_index]]
-            x = int(uv[0] * w)
-            y = int(uv[1] * h)
-            if 0 <= x < w and 0 <= y < h:
-                return f[y,x]
-    return 0
+print("Intensité du vertex 0 dans la vue 0 (canal rouge) :", get_intensity_vertice(0, 0, 0))
 
-print(intensity(0, 2)[511, 511])
-print(get_intensity_vertice(intensity(0, 2), 0, 2))
 
 def intensity_all_views(views, intensities, M):
     all_views_intensities = {}
@@ -138,54 +135,52 @@ def build_g_function(intensity_all_views, L, M, index_map, lambda_seam=100):
 
 
 
-# -- main  --
+# # -- main  --
 
-def run_channel(channel_id, color_name):
-    print(f"\n--- Traitement du canal {color_name.upper()} ---")
-    # Calculer les intensités pour toutes les vues
-    intensities = {view_id: intensity(view_id, channel_id) for view_id in range(n_views)}
-    all_views = intensity_all_views(views, intensities, M)
-    print("Intensités :", intensity(0,channel_id))
+# def run_channel(channel_id, color_name):
+#     print(f"\n--- Traitement du canal {color_name.upper()} ---")
+#     # Calculer les intensités pour toutes les vues
+#     intensities = {view_id: intensity(view_id, channel_id) for view_id in range(n_views)}
+#     all_views = intensity_all_views(views, intensities, M)
 
-    # Optimiser
-    g = build_g_function(all_views, L, M, index_map)
-    x0 = np.zeros(n)
-    res = least_squares(g, x0, jac='2-point')
-    optimal_x = res.x
-    print("Résultat de l'optimisation :", optimal_x)
+#     # Optimiser
+#     g = build_g_function(all_views, L, M, index_map)
+#     x0 = np.zeros(n)
+#     res = least_squares(g, x0, jac='2-point')
+#     optimal_x = res.x
+#     print("Résultat de l'optimisation :", optimal_x)
 
-    # Mise à jour des intensités avec la solution optimale
-    for (i, j) in all_views:
-        idx = index_map.get((i, j), None)
-        if idx is not None and (i,j) in M:
-            all_views[(i, j)] += optimal_x[idx]
-            all_views[(i, j)] = np.clip(all_views[(i, j)], 0, 255)
+#     # Mise à jour des intensités avec la solution optimale
+#     for (i, j) in all_views:
+#         idx = index_map.get((i, j), None)
+#         if idx is not None and (i,j) in M:
+#             all_views[(i, j)] += optimal_x[idx]
+#             all_views[(i, j)] = np.clip(all_views[(i, j)], 0, 255)
 
-    # Générer l'image de texture pour ce canal
-    texture_size = 512
-    texture_image = np.ones((texture_size, texture_size, 3), dtype=np.uint8) * 255
+#     # Générer l'image de texture pour ce canal
+#     texture_size = 512
+#     texture_image = np.ones((texture_size, texture_size, 3), dtype=np.uint8) * 255
 
-    for face_id, face in enumerate(mesh.faces):
-        for view_id in range(n_views):
-            h, w = texture_image.shape[:2]
-            uvs_coord = uvs[face]
-            img_coords = (uvs_coord * [w, h]).astype(int)
-            img_coords = np.clip(img_coords, 0, [w - 1, h - 1])
+#     for face_id, face in enumerate(mesh.faces):
+#         for view_id in range(n_views):
+#             h, w = texture_image.shape[:2]
+#             uvs_coord = uvs[face]
+#             img_coords = (uvs_coord * [w, h]).astype(int)
+#             img_coords = np.clip(img_coords, 0, [w - 1, h - 1])
 
-            colors = [[all_views.get((vertex, view_id), 0) if k == channel_id else 0 for k in range(3)] for vertex in face]
-            texture_triangles(uvs_coord, colors, texture_image)
+#             colors = [[all_views.get((vertex, view_id), 0) if k == channel_id else 0 for k in range(3)] for vertex in face]
+#             texture_triangles(uvs_coord, colors, texture_image)
 
-    plt.figure(figsize=(6,6))
-    plt.imshow(texture_image, origin='lower')
-    plt.axis('off')
-    plt.title(f'Texture générée ({color_name})')
-    plt.show()
-
+#         plt.figure(figsize=(6,6))
+#         plt.imshow(texture_image, origin='lower')
+#         plt.axis('off')
+#         plt.title(f'Texture générée ({color_name})')
+#         plt.show()
 
 
-# --- main ---
-# run_texture_generation()
-run_channel(0, "rouge")
-# run_channel(1, "vert")
 
-run_channel(2, "bleu")
+# # --- main ---
+# # run_texture_generation()
+# # run_channel(0, "rouge")
+# # run_channel(1, "vert")
+# # run_channel(2, "bleu")
